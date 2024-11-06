e1aa212424d7cfd95007c07b5cfd88b569a29b72
tsak <1022521+tsak@users.noreply.github.com>
2024-11-06 17:19:24 +0000

    Add methods for display buffer management and image refresh
    Introduced mutex locking to manage concurrency issues in buffer access. Added methods to get image bytes, check for updates, and refresh the display based on sensor data. The Refresh method also generates a new image and updates the display buffer accordingly.

--- display.go
+++ display.go
@@ -1,21 +1,76 @@
 package main
 
 import (
+	"bytes"
 	"github.com/fogleman/gg"
 	"image"
+	"image/png"
 	"log/slog"
+	"sync"
+	"time"
 )
 
+// Display represents a display with width, height, buffer, and last update time.
 type Display struct {
-	width  int
-	height int
+	sync.RWMutex
+	width      int
+	height     int
+	buffer     *bytes.Buffer
+	lastUpdate time.Time
 }
 
+// NewDisplay creates a new display with the specified width and height, initializing the display buffer.
 func NewDisplay(width, height int) *Display {
-	return &Display{width, height}
+	return &Display{
+		width:  width,
+		height: height,
+		buffer: bytes.NewBuffer([]byte{}),
+	}
 }
 
-func (d *Display) Image(temperature, lastModified string) (image.Image, error) {
+// GetImageBytes returns the image data as a byte slice by reading from the display buffer.
+// It will be blocked while a call to [Display.Refresh] finishes
+func (d *Display) GetImageBytes() []byte {
+	d.RLock()
+	defer d.RUnlock()
+
+	return d.buffer.Bytes()
+}
+
+// NeedsUpdate checks if the last update time is before the provided time for comparison.
+func (d *Display) NeedsUpdate(check time.Time) bool {
+	d.RLock()
+	defer d.RUnlock()
+
+	return d.lastUpdate.Before(check)
+}
+
+// Refresh generates a new image based on the provided SensorDataMessage
+// It updates the display buffer with the new image and sets the last update time
+func (d *Display) Refresh(last *SensorDataMessage) error {
+	d.Lock()
+	defer d.Unlock()
+
+	slog.Debug("Refreshing image", "temperatur", last.Temperature.String(), "date_time", last.MessageDate.String())
+
+	img, err := d.generateImage(last.Temperature.String(), last.MessageDate.String())
+	if err != nil {
+		return err
+	}
+
+	err = png.Encode(d.buffer, img)
+	if err != nil {
+		return err
+	}
+
+	d.lastUpdate = time.Time(last.MessageDate)
+
+	return nil
+}
+
+// generateImage creates an image with temperature and last modified text anchored in the center.
+// It requires temperature and lastModified strings as input, and returns the generated image.
+func (d *Display) generateImage(temperature, lastModified string) (image.Image, error) {
 	dc := gg.NewContext(d.width, d.height)
 
 	// White background
